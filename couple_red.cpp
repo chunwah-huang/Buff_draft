de055f9810cd97349f98048d90bd8cdd7ec11873
chunwah-huang <96176598@qq.com>
2019-11-14 19:57:56 +0800

    2019-11-14

--- couple_red.cpp
+++ couple_red.cpp
@@ -1,14 +1,14 @@
 #include "couple_red.h"
 
 Find_couple_red::Find_couple_red(){
-    cout << "is ready!!" << endl;
+    //cout << "is ready!!" << endl;
 }
 
 void Find_couple_red::run_Main(Mat & srcImg){
-    cout << "the pic has putout!!" << endl;
-    imshow("srcImg", srcImg);
+    //cout << "the pic has putout!!" << endl;
+    //imshow("srcImg", srcImg);
 
-    vector<cv::Mat> bgr;
+    vector<Mat> bgr;
     split(srcImg, bgr);
     Mat result_img;
     if(color_ == 0){
@@ -17,15 +17,21 @@ void Find_couple_red::run_Main(Mat & srcImg){
     else{
         subtract(bgr[0], bgr[2], result_img);
     }
-    Mat threImg;
-    threshold(result_img, threImg, 50, 255, 0);
-    imshow("threImg", threImg);
-    Mat whiteImg(srcImg.size(), srcImg.type(), Scalar(0));
+    threshold(result_img, result_img, 25, 255, 0);
+    //imshow("inputImg", result_img);
+    Mat roi_img;
+    if(is_run){
+        result_img(last_roi).copyTo(roi_img);
+    }
+    else {
+        result_img.copyTo(roi_img);
+    }
+    imshow("roi", roi_img);
 
     vector<RotatedRect> led;
     vector<vector<Point>> contour_light;
     vector<Vec4i> hierarchy;
-    findContours(threImg, contour_light, hierarchy,2,CHAIN_APPROX_NONE);
+    findContours(roi_img, contour_light, hierarchy,2,CHAIN_APPROX_NONE);
     for(int i1 = 0; i1 < (int)contour_light.size(); i1++){
         //有父,子轮廓，不满住6点拟合椭圆的先排除
         if(hierarchy[i1][3]>0 || hierarchy[i1][2]>0 || contour_light[i1].size() < 6)
@@ -37,9 +43,6 @@ void Find_couple_red::run_Main(Mat & srcImg){
         if(area < 80 || area > 1e4)
             continue;
 
-        //Rect rect = boundingRect(contour_light[i1]);
-        //rectangle(whiteImg, rect, Scalar(0,0,255), 2, LINE_8, 0);
-
         RotatedRect fit_ellipse = fitEllipse(contour_light[i1]);
 
         //归一化椭圆角度
@@ -47,95 +50,91 @@ void Find_couple_red::run_Main(Mat & srcImg){
             fit_ellipse.angle = fit_ellipse.angle - 180.0f;
         //限制角度,宽高比
         if(fabs(fit_ellipse.angle) <= 40 &&
-                (double)fit_ellipse.boundingRect().width/(double)fit_ellipse.boundingRect().height < 0.5){
+                (double)fit_ellipse.boundingRect().width/(double)fit_ellipse.boundingRect().height < 0.4){
             led.push_back(fit_ellipse);
-            ellipse(whiteImg, fit_ellipse, Scalar(0,0,255), 2, 8);
-            //cout << "size:" << led.size() << endl;
+            ellipse(srcImg, fit_ellipse, Scalar(0,0,255), 2, 8);
         }
     }
-    //排除符合轮廓数量少于2
-    if(led.size() < 2)
-        return;
 
-    vector<RotatedRect> couple_leds(2);
+    //排除符合轮廓数量少于2
     vector<vector<RotatedRect>> fit_led;
-    for(int i = 0; i < led.size(); i++){
-        for(int j = i+1; j < led.size(); j++){
-            //筛选高度,椭圆角度相似度。若条件符合，作为一对。
-            if(is_Horizon(led[i].center, led[j].center) < 0.2 && is_Aline(led[i], led[j]) < 10.0f){
-                if(led[i].center.x < led[j].center.x){
-                    couple_leds[0] = led[i];
-                    couple_leds[1] = led[j];
+    if(led.size() >= 2){
+        //        empty_count = 0;
+        //        rate_count = 1;
+        vector<RotatedRect> couple_leds(2);
+        for(int i = 0; i < led.size(); i++){
+            for(int j = i+1; j < led.size(); j++){
+                //筛选高度,椭圆角度相似度。若条件符合，作为一对。
+                if(is_Horizon(led[i].center, led[j].center) < 0.3 && is_Aline(led[i], led[j]) < 20.0f){
+                    if(led[i].center.x < led[j].center.x){
+                        couple_leds[0] = led[i];
+                        couple_leds[1] = led[j];
+                    }
+                    else {
+                        couple_leds[1] = led[i];
+                        couple_leds[0] = led[j];
+                    }
+                    fit_led.push_back(couple_leds);
+                    //cout << "num:" << fit_led.size() << endl;
                 }
-                else {
-                    couple_leds[1] = led[i];
-                    couple_leds[0] = led[j];
-                }
-                fit_led.push_back(couple_leds);
-                cout << "num:" << fit_led.size() << endl;
             }
         }
     }
 
-    double last_area;
-    double max_area;
+    //若有符合条件的每一对再进行筛选
+    RotatedRect L, R;
     double average_led;
-    vector<RotatedRect> final_couple(2);
-    vector<vector<RotatedRect>> final_led;
-    for(int k = 0; k < fit_led.size(); k++){
-        RotatedRect a_pair_l(fit_led[k][0]);
-        RotatedRect a_pair_r(fit_led[k][1]);
-        //line(whiteImg, a_pair_l.center, a_pair_r.center, Scalar(0,255,0), 2, 4, 0);
+    double dist_led;
+    succe_match = false;
+    if(fit_led.size() > 0){
+        double last_area;
+        double max_area;
+        for(int k = 0; k < fit_led.size(); k++){
+            RotatedRect a_pair_l(fit_led[k][0]);
+            RotatedRect a_pair_r(fit_led[k][1]);
+            //line(whiteImg, a_pair_l.center, a_pair_r.center, Scalar(0,255,0), 2, 4, 0);
 
-        double height_led_l = max(a_pair_l.boundingRect().height, a_pair_l.boundingRect().width);
-        double height_led_r = max(a_pair_r.boundingRect().height, a_pair_r.boundingRect().width);
-        double max_lenth_led, min_lenth_led;
-        if(height_led_l > height_led_r){
-            max_lenth_led = height_led_l;
-            min_lenth_led = height_led_r;
-        }
-        else {
-            max_lenth_led = height_led_r;
-            min_lenth_led = height_led_l;
-        }
-        //灯条相似度
-        if(min_lenth_led < 0.7*max_lenth_led)
-            continue;
+            double height_led_l = max(a_pair_l.boundingRect().height, a_pair_l.boundingRect().width);
+            double height_led_r = max(a_pair_r.boundingRect().height, a_pair_r.boundingRect().width);
+            double max_lenth_led, min_lenth_led;
+            if(height_led_l > height_led_r){
+                max_lenth_led = height_led_l;
+                min_lenth_led = height_led_r;
+            }
+            else {
+                max_lenth_led = height_led_r;
+                min_lenth_led = height_led_l;
+            }
+            //灯条相似度
+            if(min_lenth_led < 0.7*max_lenth_led)
+                continue;
 
-        average_led = (max_lenth_led + min_lenth_led)/2;
-        double dist_led = is_Dist_led(a_pair_l.center, a_pair_r.center);
-        //固定尺寸比例
-        if(dist_led > 3.0f*average_led || dist_led < 1.5*average_led)
-            continue;
-        cout << "dist:" << dist_led << "  average:" << average_led << endl;
+            average_led = (max_lenth_led + min_lenth_led)/2;
+            dist_led = is_Dist_led(a_pair_l.center, a_pair_r.center);
+            //固定尺寸比例
+            if(dist_led > 5.0f*average_led || dist_led < 1.5f*average_led)
+                continue;
+            //cout << "dist:" << dist_led << "  average:" << average_led << endl;
 
-        double area_led = average_led * dist_led;
-        max_area = (area_led > last_area ? area_led : last_area);
-        last_area = area_led;
-        //面积最大
-        if(max_area == area_led){
-            final_couple[0] = a_pair_l;
-            final_couple[1] = a_pair_r;
-            final_led.push_back(final_couple);
+            double area_led = average_led * dist_led;
+            max_area = (area_led > last_area ? area_led : last_area);
+            last_area = area_led;
+            //面积最大
+            if(max_area == area_led){
+                L = a_pair_l;
+                R = a_pair_r;
+                succe_match = true;
+            }
         }
-        line(whiteImg, final_couple[0].center, final_couple[1].center, Scalar(0,255,0), 2, 4, 0);
     }
-
-    Mat roi_rect = threImg;
-    if(final_led.size() > 0){
-        Point2f center_rect((final_couple[0].center.x +final_couple[1].center.x)/2, (final_couple[0].center.y +final_couple[1].center.y)/2);
-        double up_limit_y = (center_rect.y - 3.0f*average_led < 0 ? 0: center_rect.y - 3.0f*average_led);
-        roi_rect = threImg(Range(up_limit_y, center_rect.y), Range(final_couple[0].center.x, final_couple[1].center.x));
-    }
-
-    line(whiteImg, final_couple[0].center, final_couple[1].center, Scalar(0,0,255), 2, 4, 0);
-    imshow("roi", roi_rect);
-    imshow("output", whiteImg);
+    get_Roi(succe_match, result_img, L, R, dist_led, average_led, srcImg);
+    rectangle(srcImg, last_roi, Scalar(0, 255, 0), 2, 8, 0);
+    imshow("output", srcImg);
 }
 
 double Find_couple_red::is_Horizon(Point2f & center_i, Point2f & center_j){
     double gradient = fabs((center_i.y - center_j.y)/(center_i.x - center_j.x));
-    cout << "gradient:" << gradient << endl;
+    //cout << "gradient:" << gradient << endl;
     return gradient;
 }
 
@@ -167,6 +166,93 @@ double Find_couple_red::is_Dist_led(Point2f & point_i, Point2f & point_j){
     double dist = pow(pow(point_i.x - point_j.x, 2) + pow(point_i.y - point_j.y, 2), 0.5);
 
     return dist;
+}
+
+RotatedRect Find_couple_red::boundRect(RotatedRect & L_led, RotatedRect & R_led, double & W, double & H){
+
+    float angle = atan2(R_led.center.y - L_led.center.y, R_led.center.x - L_led.center.x);
+
+    Point2f center((L_led.center.x +R_led.center.x)/2, (L_led.center.y +R_led.center.y)/2);
+    //cout << "angle:" << angle * 180 / CV_PI << endl;
+    return RotatedRect(center, Size2f(W, H), angle * 180 / CV_PI);
+}
+
+Rect Find_couple_red::get_Roi(bool & is_target, Mat & img, RotatedRect & l, RotatedRect & r, double & dis, double & aver, Mat & src){
+    if(is_target){
+        lost_cnt = 0;
+        //识别坐标并转换
+        center_point.x = (l.center.x + r.center.x)/2 + last_roi.x;
+        center_point.y = (l.center.y + r.center.y)/2 + last_roi.y;
+        //保留last_Rotated
+        bound_rotated = boundRect(l, r, dis, aver);
+        circle(src, center_point, 3, Scalar(0, 255, 0), 3, 8, 0);
+    }
+    else {
+        if(bound_rotated.size.width > 0 || bound_rotated.size.height > 0){
+            ++lost_cnt;
+            bool is_increase = false;
+            if(lost_cnt < 6)
+                bound_rotated.size = Size2f(bound_rotated.size.width, bound_rotated.size.height);
+            else if(lost_cnt == 6){
+                bound_rotated.size = Size2f(bound_rotated.size.width*1.5, bound_rotated.size.height*1.5);
+                is_increase = true;
+            }
+            else if(lost_cnt == 10){
+                bound_rotated.size = Size2f(bound_rotated.size.width*1.5, bound_rotated.size.height*1.5);
+                is_increase = true;
+            }
+            else if(lost_cnt == 14){
+                bound_rotated.size = Size2f(bound_rotated.size.width*1.5, bound_rotated.size.height*1.5);
+                is_increase = true;
+            }
+            else if(lost_cnt == 18){
+                bound_rotated.size = Size2f(bound_rotated.size.width*1.5, bound_rotated.size.height*1.5);
+                is_increase = true;
+            }
+            else if(lost_cnt > 22){
+                bound_rotated = RotatedRect();
+                lost_cnt = 0;
+            }
+
+            if(is_increase){
+                make_RotatedSafe(img);
+            }
+        }
+        else {
+            bound_rotated = RotatedRect();
+        }
+    }
+
+    if(bound_rotated.size.width > 0 || bound_rotated.size.height > 0){
+        Rect rect = bound_rotated.boundingRect();
+        int W, H;
+        W = rect.width*2;
+        H = rect.height*4;
+        int x, y;
+        x = MAX(center_point.x-W*0.5, 0);
+        y = MAX(center_point.y-H*0.5, 0);
+        Point TL = Point(x, y);
+        x = MIN(center_point.x+W*0.5, img.cols);
+        y = MIN(center_point.y+H*0.5, img.rows);
+        Point BR = Point(x, y);
+        last_roi = Rect(TL, BR);
+    }
+    else {
+        last_roi = Rect(0, 0, img.cols, img.rows);
+    }
+    is_run = true;
+    return last_roi;
+}
+
+void Find_couple_red::make_RotatedSafe(Mat & bound_size){
+    Point2f vertex[4];
+    bound_rotated.points(vertex);
+    sort(vertex, vertex + 4, [](const Point2f & p1, const Point2f & p2) {return p1.x < p2.x;});
+    if(vertex[0].x < 0 || vertex[4].x > bound_size.cols)
+        bound_rotated = RotatedRect();
+    sort(vertex, vertex + 4, [](const Point2f & p3, const Point2f & p4) {return p3.y < p4.y;});
+    if(vertex[0].y < 0 || vertex[4].y > bound_size.rows)
+        bound_rotated = RotatedRect();
 }
 
 Find_couple_red::~Find_couple_red(){
--- main.cpp
+++ main.cpp
@@ -1,38 +1,122 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include "CameraApi.h"
 #include "couple_red.h"
+#include <opencv2/imgproc/imgproc_c.h>
 using namespace std;
 using namespace cv;
 
 int main()
 {
-    //"/home/hzh/图片/2020-能量机关.png"
-    Mat src = imread("/home/hzh/图片/2020-buff(复件).png", 1);
-    //imshow("src", src);
-    //resize(src, src, Size(640, 480));
+    VideoCapture cap(1);
+    unsigned char *g_pRgbBuffer; //处理后数据缓存区
+
+    int                     iCameraCounts = 1;
+    int                     iStatus=-1;
+    tSdkCameraDevInfo       tCameraEnumList;
+    int                     hCamera;
+    tSdkCameraCapbility     tCapability;      //设备描述信息
+    tSdkFrameHead           sFrameInfo;
+    BYTE*			        pbyBuffer;
+    IplImage                *iplImage = NULL;
+    int                     channel=3;
+    BOOL                    AEstate=FALSE;
+
+    CameraSdkInit(1);
+    //枚举设备，并建立设备列表
+    iStatus = CameraEnumerateDevice(&tCameraEnumList,&iCameraCounts);
+    printf("state = %d\n", iStatus);
+    printf("count = %d\n", iCameraCounts);
+    //没有连接设备
+    if(iCameraCounts==0)
+    {
+        return -1;
+    }
+    //相机初始化。初始化成功后，才能调用任何其他相机相关的操作接口
+    iStatus = CameraInit(&tCameraEnumList,-1,-1,&hCamera);
+    //初始化失败
+    printf("state = %d\n", iStatus);
+    if(iStatus!=CAMERA_STATUS_SUCCESS)
+    {
+        return -1;
+    }
+    //获得相机的特性描述结构体。该结构体中包含了相机可设置的各种参数的范围信息。决定了相关函数的参数
+    CameraGetCapability(hCamera,&tCapability);
+    g_pRgbBuffer = (unsigned char*)malloc(tCapability.sResolutionRange.iHeightMax*tCapability.sResolutionRange.iWidthMax*3);
+    cout<<CameraGetAeState(hCamera,&AEstate);
+    cout<<CameraSetAeState(hCamera,FALSE);
+
+    CameraSetExposureTime(hCamera,33000);
+    /*让SDK进入工作模式，开始接收来自相机发送的图像数据。
+     *如果当前相机是触发模式，则需要接收到触发帧以后才会更新图像*/
+    CameraPlay(hCamera);
+    /*
+    其他的相机参数设置
+    例如 CameraSetExposureTime   CameraGetExposureTime  设置/读取曝光时间
+         CameraSetImageResolution  CameraGetImageResolution 设置/读取分辨率
+         CameraSetGamma、CameraSetConrast、CameraSetGain等设置图像伽马、对比度、RGB数字增益等等。
+         更多的参数的设置方法，，清参考MindVision_Demo。本例程只是为了演示如何将SDK中获取的图像，转成OpenCV的图像格式,以便调用OpenCV的图像处理函数进行后续开发
+    */
+    if(tCapability.sIspCapacity.bMonoSensor)
+    {
+        channel=1;
+        CameraSetIspOutFormat(hCamera,CAMERA_MEDIA_TYPE_MONO8);
+    }else
+    {
+        channel=3;
+        CameraSetIspOutFormat(hCamera,CAMERA_MEDIA_TYPE_BGR8);
+    }
+
     Find_couple_red RM_dist;
-    RM_dist.run_Main(src);
+    while(true)
+    {
+        if(CameraGetImageBuffer(hCamera,&sFrameInfo,&pbyBuffer,1000) == CAMERA_STATUS_SUCCESS)
+        {
 
-//    VideoCapture cap;
-//    cap.open(1);
+            CameraImageProcess(hCamera, pbyBuffer, g_pRgbBuffer,&sFrameInfo);
+            if (iplImage)
+            {
+                cvReleaseImageHeader(&iplImage);
+            }
+            iplImage = cvCreateImageHeader(cvSize(sFrameInfo.iWidth,sFrameInfo.iHeight),IPL_DEPTH_8U,channel);
+            cvSetData(iplImage,g_pRgbBuffer,sFrameInfo.iWidth*channel);//此处只是设置指针，无图像块数据拷贝，不需担心转换效率
 
-//    if(!cap.isOpened())
-//        cout << "can not open the cap!" << endl;
-//    return -1;
+            //Mat Iimag(iplImage);//这里只是进行指针转换，将IplImage转换成Mat类型
+            Mat src_img = cvarrToMat(iplImage,true);
+            resize(src_img, src_img, Size(640, 480));
+            RM_dist.run_Main(src_img);
+            //imshow("src", src_img);
 
-//    Find_couple_red RM_dist;
-//    Mat frame;
-//    while(1){
-//        cap >> frame;
-//        imshow("frame", frame);
-//        resize(frame, frame, Size(640, 480));
-//        RM_dist.run_Main(frame);
+            //在成功调用CameraGetImageBuffer后，必须调用CameraReleaseImageBuffer来释放获得的buffer。
+            //否则再次调用CameraGetImageBuffer时，程序将被挂起一直阻塞，直到其他线程中调用CameraReleaseImageBuffer来释放了buffer
+            CameraReleaseImageBuffer(hCamera,pbyBuffer);
+            char k  = waitKey(1);
+            if(k == 27)
+            {
+                break;
+            }
 
-//        char c = waitKey(1);
-//        if(c == 27){
-//            break;
-//        }
-//    }
-    waitKey(0);
+        }
+    }
+
+    CameraUnInit(hCamera);
+    //注意，现反初始化后再free
+    free(g_pRgbBuffer);
+
     return 0;
 }
+
+
+
+//int main()
+//{
+//    //"/home/hzh/图片/2020-能量机关.png"
+//    Mat src = imread("/home/hzh/图片/2020-buff(复件).png", 1);
+//    //imshow("src", src);
+//    //resize(src, src, Size(640, 480));
+//    Find_couple_red RM_dist;
+//    RM_dist.run_Main(src);
+
+//    waitKey(0);
+//    return 0;
+//}
